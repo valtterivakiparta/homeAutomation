98bf2efe303decc5a041e7f8ef9971175c99c6e3
Valtteri Väkiparta <valtteri.vakiparta@icloud.com>
2023-03-15 01:27:57 +0200

    getKWh prices problem solved. now it works 24/7

--- python/getKWh/main.py
+++ python/getKWh/main.py
@@ -1,22 +1,32 @@
 import datetime
 from classes.connectToDatabase import connect
+from classes.classes import sendSMS
+import os
 
 date = datetime.datetime.now()
 nextDayDate = date + datetime.timedelta(days=1)
 getData = connect()
 timer = 0
+SMS = sendSMS()
 
 print("Connected and waiting nextday prices.")
 while True: 
-    rightTime = datetime.datetime.now().time().replace(microsecond=0)
     if __name__ == "__main__":
-        if rightTime.hour == 14 and rightTime.minute == 00 and rightTime.second == 00:
-            date = datetime.datetime.now()
-            nextDayDate = date + datetime.timedelta(days=1)
-            while timer < 24:
-                getData.dbconnect(nextDayDate.date(), timer)
-                timer = timer + 1
-
+        rightTime = datetime.datetime.now().time().replace(microsecond=0)
+        if rightTime.hour == 17 and rightTime.minute == 00 and rightTime.second == 00:
+            os.system("clear")
+            print("Connected and waiting nextday prices.")
+            timer = 0
+            try:
+                date = datetime.datetime.now()
+                nextDayDate = date + datetime.timedelta(days=1)
+                print("Hinta tunnilta", timer)
+                while timer < 24:
+                    print(timer)
+                    getData.dbconnect(nextDayDate.date(), timer)
+                    timer = timer + 1
+            except: 
+                SMS.sendSMS()
 
 
 
